48b16ee9261350881467fa513bda87fa87fff2a2
NTH <propritaire@192.168.1.14>
2015-02-01 15:55:43 +0100

    Add mutual code already written (classes in package data and interface)
    Add exception classes linked with features account creation and
    authentication.

---
+++ src/data/Profile.java
@@ -0,0 +1,43 @@
+package data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Profile
+{
+	private List<Tag> tags;
+	private String name;
+	
+	public Profile(String name)
+	{
+		this.name = name;
+		tags = new ArrayList<>();
+	}
+	
+	String getName()
+	{
+		return name;
+	}
+
+
+
+	public List<Tag> getTags()
+	{
+		return tags;
+	}
+	
+	public boolean addTag(Tag tag)
+	{
+		return tags.add(tag);
+	}
+	
+	public boolean removeTag(Tag tag)
+	{
+		return tags.remove(tag);
+	}
+	
+	public void removeAllTags()
+	{
+		tags.clear();
+	}
+}
