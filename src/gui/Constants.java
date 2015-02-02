f614621d4bad4e08011e7edeafc33672912b2d83
NTH <propritaire@192.168.1.14>
2015-02-02 20:22:08 +0100

    New class Constants : class which contains all string constants (labels, ...)

---
+++ src/gui/Constants.java
@@ -0,0 +1,9 @@
+package gui;
+
+public class Constants {
+	public static class MainFrame{
+		public static final String USER_INFORMATIONS_PANEL_NAME = "User informations";
+		public static final String TAGS_PANEL_NAME = "Tags management";
+		public static final String PROFILES_PANEL_NAME = "Profiles management";
+	}
+}
