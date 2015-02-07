4e97ffef3201fe2260d81973643864ce3232aa8d
NTH <propritaire@192.168.1.14>
2015-02-07 22:09:51 +0100

    Add class NetworkServiceProvider : class which contains the reference of the object corresponding to the network service. class EmulatorTest modified to use NetworkServiceProvider.

--- src/tests/EmulatorTest.java
+++ src/tests/EmulatorTest.java
@@ -3,23 +3,23 @@ package tests;
 import java.util.Date;
 
 import data.Account;
+import engine.NetworkServiceProvider;
 import exceptions.AccountNotFoundException;
 import exceptions.IllegalFieldException;
 import exceptions.NetworkServiceException;
-import interfaces.NetworkServiceInterface;
 
 public class EmulatorTest
 {
 	@SuppressWarnings("deprecation")
 	public static void main(String[] args)
 	{
-		NetworkServiceInterface service = NetworkServiceEmulator.getInstance();
+		NetworkServiceProvider.setNetworkService(NetworkServiceEmulator.getInstance());
 		
 		Account account;
 		try {
 			account = new Account("nth", "Nicolas", "Thierce", new Date(1993, 10, 20), "nth@gmail.com");
 			try {
-				service.createAccount(account, "notOk");
+				NetworkServiceProvider.getNetworkService().createAccount(account, "notOk");
 				System.out.println("account created.");
 			} catch (IllegalFieldException e) {
 				System.out.println("error for account creation : " + e.getMessage());
@@ -27,7 +27,7 @@ public class EmulatorTest
 				e.printStackTrace();
 			}
 			try {
-				service.createAccount(account, "correctPassword");
+				NetworkServiceProvider.getNetworkService().createAccount(account, "correctPassword");
 				System.out.println("account created.");
 			} catch (IllegalFieldException e) {
 				System.out.println("error for account creation : " + e.getMessage());
@@ -37,7 +37,7 @@ public class EmulatorTest
 			
 			// will fail because pseudo already used.
 			try {
-				service.createAccount(account, "correctPassword");
+				NetworkServiceProvider.getNetworkService().createAccount(account, "correctPassword");
 				System.out.println("account created.");
 			} catch (IllegalFieldException e) {
 				System.out.println("error for account creation : " + e.getMessage());
@@ -50,9 +50,9 @@ public class EmulatorTest
 		}
 
 		try {
-			service.authenticate("invalid", "invalid");
+			NetworkServiceProvider.getNetworkService().authenticate("invalid", "invalid");
 			System.out.println("Authentication 1 done.");
-			service.logOut();
+			NetworkServiceProvider.getNetworkService().logOut();
 		} catch (AccountNotFoundException e) {
 			System.out.println("account not found.");
 		} catch (NetworkServiceException e) {
@@ -60,7 +60,7 @@ public class EmulatorTest
 		}
 
 		try {
-			service.authenticate("nth", "correctPassword");
+			NetworkServiceProvider.getNetworkService().authenticate("nth", "correctPassword");
 			System.out.println("Authentication 2 done.");
 		} catch (AccountNotFoundException e) {
 			System.out.println("account not found.");
