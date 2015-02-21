41df1f87d17cf7e9810fade0819142afc4d278be
NTH <propritaire@192.168.1.17>
2015-02-21 23:04:06 +0100

    class MainFrameTest : corrected (call to method initializeNetworkService() added).

--- src/tests/gui/MainFrameTest.java
+++ src/tests/gui/MainFrameTest.java
@@ -2,6 +2,7 @@ package tests.gui;
 
 import tests.NetworkServiceEmulator;
 import engine.NetworkServiceProvider;
+import exceptions.NetworkServiceException;
 import gui.MainFrame;
 
 public class MainFrameTest
@@ -9,7 +10,11 @@ public class MainFrameTest
 	public static void main(String args[])
 	{
 		NetworkServiceProvider.setNetworkService(NetworkServiceEmulator.getInstance());
-		
-		new MainFrame();
+		try {
+			NetworkServiceProvider.getNetworkService().initNetworkService();
+			new MainFrame();
+		} catch (NetworkServiceException e) {
+			e.printStackTrace();
+		}
 	}
 }
