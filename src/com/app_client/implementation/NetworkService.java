2afb0ec9a8c1dd3e549ba73116dd22d8a3357316
dopzac <heurtauxphil@gmail.com>
2015-02-22 17:02:14 +0100

    New version of login with error treatment (still not complete we have to define the rules for the JSON)

--- src/com/app_client/implementation/NetworkService.java
+++ src/com/app_client/implementation/NetworkService.java
@@ -2,18 +2,18 @@ package com.app_client.implementation;
 
 import java.util.List;
 
-import org.apache.http.Header;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import com.app_client.data.Account;
 import com.app_client.data.Profile;
 import com.app_client.data.Tag;
-
+import com.app_client.engine.FieldVerifier;
 import com.app_client.exceptions.AccountNotFoundException;
 import com.app_client.exceptions.IllegalFieldException;
 import com.app_client.exceptions.NetworkServiceException;
 import com.app_client.exceptions.NotAuthenticatedException;
 import com.app_client.interfaces.NetworkServiceInterface;
-
 import com.loopj.android.http.*;
 
 public class NetworkService implements NetworkServiceInterface {
@@ -29,25 +29,85 @@ public class NetworkService implements NetworkServiceInterface {
 	@Override
 	public void createAccount(Account newAccount, String newPassword)
 			throws IllegalFieldException, NetworkServiceException 
-	{
+	{	// We first check the validity of the arguments to create the parameters
 		RequestParams params = new RequestParams();
-		params.put("pseudo", newAccount.getPseudo());
-		params.put("password", newPassword);
-		params.put("first_name", newAccount.getFirstName());
-		params.put("last_name", newAccount.getLastName());
-		params.put("email", newAccount.getEMailAddress());
+		if (FieldVerifier.verifyName(newAccount.getPseudo())){
+			params.put("pseudo", newAccount.getPseudo());}
+		else { throw new IllegalFieldException(IllegalFieldException.PSEUDO, null);}
+		
+		if (FieldVerifier.verifyPassword(newPassword)){
+			params.put("password", newPassword);}
+		else { throw new IllegalFieldException(IllegalFieldException.PSEUDO, null);}
+		
+		if (FieldVerifier.verifyName(newAccount.getFirstName())){
+			params.put("first_name", newAccount.getFirstName());}
+		else { throw new IllegalFieldException(IllegalFieldException.FIRSTNAME, null);}
+		
+		if (FieldVerifier.verifyName(newAccount.getLastName())){
+			params.put("last_name", newAccount.getLastName());}
+		else { throw new IllegalFieldException(IllegalFieldException.LASTNAME, null);}
+		
+		if (FieldVerifier.verifyEMailAddress(newAccount.getEMailAddress())){
+			params.put("email", newAccount.getEMailAddress());}
+		else { throw new IllegalFieldException(IllegalFieldException.EMAIL_ADDRESS, null);}
+		
 				
-		client.get("http://www.localhost:9999/dologin/login/", params, new AsyncHttpResponseHandler() {	
+		client.get("http://www.localhost:8080/ns/dologin", params, new AsyncHttpResponseHandler() {	
 
-		    @Override
-		    public void onSuccess(int statusCode, Header[] headers, byte[] response) {
-		        // called when response HTTP status is "200 OK"
-		    }
+			// When the response returned by REST has Http response code '200'
+            @Override
+            public void onSuccess(String response) {
+                try {
+                         // JSON Object
+                        JSONObject obj = new JSONObject(response);
+                        // When the JSON response has status boolean value assigned with true
+                        if(obj.getBoolean("status")){
+                            //
+                        } 
+                        // Else display error message
+                        else{
+                           //
+                        }
+                } catch (JSONException e) {
+                    // TODO Auto-generated catch block
+                    // "Error Occured [Server's JSON response might be invalid]!"
+                    e.printStackTrace();
 
-		    @Override
-		    public void onFailure(int statusCode, Header[] headers, byte[] errorResponse, Throwable e) {
-		        // called when response HTTP status is "4XX" (eg. 401, 403, 404)
-		    }
+                }
+            }
+
+		 // When the response returned by REST has Http response code other than '200'
+            @Override
+            public void onFailure(int statusCode, Throwable error,
+                String content) {             
+                // When Http response code is '404'
+                if(statusCode == 404){
+                  try {
+					throw new NetworkServiceException("Requested resource not found");
+				} catch (NetworkServiceException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+                } 
+                // When Http response code is '500'
+                else if(statusCode == 500){
+                   try {
+					throw new NetworkServiceException("Something went wrong at server end");
+				} catch (NetworkServiceException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+                } 
+                // When Http response code other than 404, 500
+                else{
+                 try {
+					throw new NetworkServiceException("Unexpected Error occcured! [Most common Error: Device might not be connected to Internet or remote server is not up and running]");
+				} catch (NetworkServiceException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+                }
+            }
 		    			
 		});	
 	}
