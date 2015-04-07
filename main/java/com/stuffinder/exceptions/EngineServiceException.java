93460296fc37e8f14cce32fbb51f8f5f6eb58311
hlepage <henri.lepage@telecom-paristech.fr>
2015-04-07 14:11:22 +0200

    Merge branch 'WebServiceBranch' into HenriBranch (reverted from commit 2ce2777612b201f132fa0ff15e0009ed8fbe3b2c)

--- main/java/com/stuffinder/exceptions/EngineServiceException.java
+++
@@ -1,8 +0,0 @@
-package com.stuffinder.exceptions;
-
-/**
- * Created by propriétaire on 15/03/2015.
- */
-public class EngineServiceException extends Exception {
-
-}
--- main/java/com/stuffinder/webservice/ConstantsWebService.java
+++
@@ -1,7 +0,0 @@
-package com.stuffinder.webservice;
-
-public class ConstantsWebService {
-
-    public final static String server_address = "http://92.222.33.38:8080/app_server/ns/";
-
-}
